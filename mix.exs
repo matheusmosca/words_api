296ad2be26254c4c42c68021ba46e6ac3069b89b
Matheus Mosca <matheusmoscag@gmail.com>
2022-04-17 11:30:37 -0300

    chore: add dependencies

--- mix.exs
+++ mix.exs
@@ -22,6 +22,13 @@ defmodule Words.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:phoenix, "~> 1.6.6"},
+      {:postgrex, ">= 0.0.0"},
+      {:swoosh, "~> 1.3"},
+      {:gettext, "~> 0.18"},
+      {:hackney, ">= 1.18.0"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
